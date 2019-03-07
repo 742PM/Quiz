693e16cf1752d22f21e01d4bde74372e563413e9
Anton Voitsishevskii <grygry0@gmail.com>
2019-03-07 14:38:47 +0300

    fill domain up;

--- Complexity/Domain/Level.cs
+++ Complexity/Domain/Level.cs
@@ -1,14 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Domain
 {
+    [Entity]
     public class Level
     {
-        public Level(IEnumerable<Task> tasks)
+        public Level(IEnumerable<Task> tasks, Guid id, Dictionary<Task, int> taskWeights)
         {
-            Tasks = tasks;
+            Tasks = new HashSet<Task>(tasks);
+            Id = id;
+            TaskWeights = taskWeights;
         }
 
-        public IEnumerable<Task> Tasks { get; }
+        public Guid Id { get; }
+
+        public HashSet<Task> Tasks { get; }
+
+        public int CurrentProgress =>
+            Tasks.Select(t => TaskWeights[t])
+                 .Sum() -
+            SolvedTasks.Select(t => TaskWeights[t])
+                       .Sum();
+
+        public HashSet<Task> SolvedTasks { get; } = new HashSet<Task>();
+
+        public Dictionary<Task, int> TaskWeights { get; }
+
+        public Level Solve(Task task)
+        {
+            SolvedTasks.Add(task);
+            return this;
+        }
     }
 }

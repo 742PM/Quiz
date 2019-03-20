fc6eb15a4f04881b02ffdedaa7daf414aa911ce3
insomnia* <budljanskiy@mail.ru>
2019-03-20 19:49:50 +0500

    Removed AnswerEntity; Replaced into strings;

--- Complexity/DataBase/DataTransferObjects/TaskEntity.cs
+++ Complexity/DataBase/DataTransferObjects/TaskEntity.cs
@@ -8,12 +8,11 @@ namespace DataBase
         public Guid GeneratorId { get; set; }
 
         public int Difficulty { get; set; }
-        
-        public AnswerEntity RightAnswer { get; set; }
-        public AnswerEntity[] Answers { get; set; }
-        
+
+        public string RightAnswer { get; set; }
+        public string[] Answers { get; set; }
+
         public string[] Hints { get; set; }
         public string Question { get; set; }
-        
     }
 }

141522d40bf76414778d6ab1b398af47698678ec
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2019-03-20 23:21:03 +0500

    add test task generator

---
+++ Complexity/Tests/TestGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain;
+
+namespace Tests
+{
+    public class TestGenerator : ITaskGenerator
+    {
+        public TestGenerator(int difficulty)
+        {
+            Difficulty = difficulty;
+        }
+
+        public IEnumerable<Task> Tasks => new[]
+        {
+            new Task(new[] { "a1", "a2", "a3" }, "?", new[] { "h1", "h2", "h3" }, "a1", Id),
+        };
+
+        public string Description => "Test";
+
+        public int Difficulty { get; }
+
+        public Guid Id => Guid.NewGuid();
+    }
+}

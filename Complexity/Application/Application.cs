053a7bf16ab9a03ad665948859aacc0e32536a11
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2019-03-20 23:22:10 +0500

    fix GetDifficulties

--- Complexity/Application/Application.cs
+++ Complexity/Application/Application.cs
@@ -14,10 +14,10 @@ namespace Application
         {
             var container = new StandardKernel();
 
-            container.Bind<IQuizApi>()
-                .To<QuizApi>()
-                .InSingletonScope()
-                .WithConstructorArgument("id", Guid.Empty);
+            //container.Bind<IQuizApi>()
+            //    .To<QuizApi>()
+            //    .InSingletonScope()
+            //    .WithConstructorArgument("id", Guid.Empty);
             container.Bind<ITaskGenerator>()
                 .To<ExampleTaskGenerator>()
                 .WithConstructorArgument("hint", "Wow"); //Какое-то странное что-то, просто пример

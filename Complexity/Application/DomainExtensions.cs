3766b9a8d4c7713e871c6bee0ded80e62fa99cb9
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2019-03-21 23:08:30 +0500

    update domain extensions

--- Complexity/Application/DomainExtensions.cs
+++ Complexity/Application/DomainExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Info;
+using DataBase;
+using Domain;
 
 namespace Application
 {
@@ -7,5 +9,19 @@ namespace Application
         public static TaskInfo ToInfo(this Task task) => new TaskInfo(task.Question, task.Answers);
 
         public static TopicInfo ToInfo(this Topic topic) => new TopicInfo(topic.Name, topic.Id);
+
+        public static TaskEntity ToEntity(this Task task, ITaskGenerator generator)
+        {
+            var (question, answers, hints, rightAnswer) = task;
+            return new TaskEntity
+            {
+                Answers = answers,
+                Difficulty = generator.Difficulty,
+                GeneratorId = generator.Id,
+                Hints = hints,
+                Question = question,
+                RightAnswer = rightAnswer
+            };
+        }
     }
 }

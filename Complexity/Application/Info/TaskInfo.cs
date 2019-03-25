316d8c14347c07a01a199639fb8bd9f1ff9d6884
Artemiy <art.izakov@gmail.com>
2019-03-25 13:46:52 +0500

    interface update

--- Complexity/Application/Info/TaskInfo.cs
+++ Complexity/Application/Info/TaskInfo.cs
@@ -2,14 +2,13 @@
 {
     public class TaskInfo
     {
+        public string Question { get; }
+        public string[] Answers { get; }
+
         public TaskInfo(string question, string[] answers)
         {
             Question = question;
             Answers = answers;
         }
-
-        public string Question { get; }
-
-        public string[] Answers { get; }
     }
 }

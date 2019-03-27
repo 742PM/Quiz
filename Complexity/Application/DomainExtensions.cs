2cbee3c5a86d9fdbb8cc2bfc67cbbf007af9d098
Artemiy <art.izakov@gmail.com>
2019-03-28 00:19:17 +0500

    fix domain extensions

--- Complexity/Application/DomainExtensions.cs
+++ Complexity/Application/DomainExtensions.cs
@@ -9,21 +9,10 @@ namespace Application
 {
     public static class DomainExtensions
     {
-        public static TaskInfo ToInfo(this Task task) => new TaskInfo(task.Question, null); //TODO: remove stub
-
         public static TopicInfo ToInfo(this Topic topic) => new TopicInfo(topic.Name, topic.Id);
 
-        //public static TaskEntity ToEntity(this Task task, TaskGenerator generator)
-        //{
-        //    var (question, hints, rightAnswer) = task;
-        //    return new TaskEntity
-        //    {
-        //        //Difficulty = generator.Difficulty,
-        //        GeneratorId = generator.Id,
-        //        Hints = hints,
-        //        Question = question,
-        //        Answer = rightAnswer
-        //    };
-        //}
+        public static LevelInfo ToInfo(this Level level) => new LevelInfo(level.Id, level.Description);
+
+        public static TaskInfo ToInfo(this Task task) => new TaskInfo(task.Question, task.PossibleAnswers);
     }
 }

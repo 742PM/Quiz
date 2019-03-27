908f6513237eea9461f80ddb7b8c985227c57708
insomnia* <budljanskiy@mail.ru>
2019-03-27 15:56:30 +0500

    Starting of work;

--- Complexity/DataBase/Program.cs
+++ Complexity/DataBase/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using Domain.Entities.TaskGenerators;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 namespace DataBase.Entities
@@ -11,7 +16,47 @@ namespace DataBase.Entities
                                         "mongodb://localhost:27017";
             var db = new MongoClient(mongoConnectionString).GetDatabase("ComplexityBot");
             var userRepo = new MongoUserRepository(db);
-            var user = new UserEntity(new Guid(), new UserProgressEntity());
+
+            BsonClassMap.RegisterClassMap<TaskGenerator>(tg =>
+            {
+                tg.AutoMap();
+                tg.SetIsRootClass(true);
+            });
+
+            BsonClassMap.RegisterClassMap<TaskGenerator>();
+            BsonClassMap.RegisterClassMap<TemplateTaskGenerator>();
+
+            BsonClassMap.RegisterClassMap<LevelProgressEntity>(lpe =>
+            {
+                lpe.AutoMap();
+                lpe.MapMember(c => c.CurrentLevelStreaks).SetSerializer(
+                    new DictionaryInterfaceImplementerSerializer<Dictionary<Guid, int>>(DictionaryRepresentation
+                        .ArrayOfDocuments));
+            });
+
+            BsonClassMap.RegisterClassMap<TopicProgressEntity>(tpe =>
+            {
+                tpe.AutoMap();
+                tpe.MapMember(c => c.LevelProgressEntities).SetSerializer(
+                    new DictionaryInterfaceImplementerSerializer<Dictionary<Guid, LevelProgressEntity>>(
+                        DictionaryRepresentation.ArrayOfDocuments));
+            });
+
+            BsonClassMap.RegisterClassMap<UserProgressEntity>(upe =>
+            {
+                upe.AutoMap();
+                upe.MapMember(c => c.TopicsProgress).SetSerializer(
+                    new DictionaryInterfaceImplementerSerializer<Dictionary<Guid, TopicProgressEntity>>(
+                        DictionaryRepresentation.ArrayOfDocuments));
+            });
+            var levelProgressEntity = new LevelProgressEntity
+                {CurrentLevelStreaks = new Dictionary<Guid, int>{{new Guid(), 10 }}};
+            
+            var topicProgressEntity = new TopicProgressEntity
+                {LevelProgressEntities = new Dictionary<Guid, LevelProgressEntity>{{new Guid(), levelProgressEntity }}};
+            var userProgress = new UserProgressEntity
+                {TopicsProgress = new Dictionary<Guid, TopicProgressEntity> {{new Guid(), topicProgressEntity}}};
+            var user = new UserEntity(new Guid(), userProgress);
 
             userRepo.Insert(user);
         }

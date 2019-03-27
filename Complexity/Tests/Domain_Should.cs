7f2e895a31fcee243887e0cf698b2236fb0546d5
Anton Voitsishevskii <grygry0@gmail.com>
2019-03-27 19:24:50 +0300

    fix domain tests;

--- Complexity/Tests/Domain_Should.cs
+++ Complexity/Tests/Domain_Should.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Domain;
 using Domain.Entities;
 using Domain.Entities.TaskGenerators;
 using FluentAssertions;
@@ -13,24 +12,27 @@ namespace Tests
         [Test]
         public void AcceptItself()
         {
+            const string whoa = "Whoa";
             new[]
                 {
                     new Topic(Guid.Empty, "Some Topic", "Some description",
                               new[]
                               {
                                   new Level(Guid.Empty, "Yet another description",
-                                            new (TaskGenerator, int)[]
+                                            new TaskGenerator[]
                                             {
-                                                (new ExampleTaskGenerator("Woah!", Guid.Empty), 3),
-                                                (new TemplateTaskGenerator(Guid.Empty, new string[0], "Woah! That is some real code!", new string[0], "Woah! This is an ANSWER!"),
-                                                 2),
-                                                (new ExampleTaskGenerator("Woah!", Guid.Empty), 1)
+                                                new ExampleTaskGenerator($"{whoa}!", Guid.Empty),
+                                                new TemplateTaskGenerator(Guid.Empty, new string[0],
+                                                                          $"{whoa}! That is some real code!",
+                                                                          new string[0],
+                                                                          $"{whoa}! This is an ANSWER!", 2),
+                                                new ExampleTaskGenerator($"{whoa}!", Guid.Empty)
                                             })
                               })
                 }.Should()
-                 .NotContainNulls() 
+                 .NotContainNulls()
 //               .And.BeFine();
-;
+                ;
         }
     }
 }

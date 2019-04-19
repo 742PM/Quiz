5827dc68d54821dd1facdac56416130ce9882e0e
Anton Voitsishevskii <grygry0@gmail.com>
2019-04-19 14:49:48 +0300

    refactor db setup;

--- Complexity/DataBase/MongoDatabaseInitializer.cs
+++ Complexity/DataBase/MongoDatabaseInitializer.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Application.Repositories;
 using Application.Repositories.Entities;
 using Domain.Entities;
 using Domain.Entities.TaskGenerators;
 using Infrastructure.DDD;
+using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Options;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
@@ -13,117 +18,94 @@ namespace DataBase
 {
     public static class MongoDatabaseInitializer
     {
-        public static IMongoDatabase Connect(string databaseName, string username = default, string password = default)
+
+        public static IMongoDatabase CreateMongoDatabase(string databaseName, string username = default, string password = default)
+        {
+             SetupDatabase();
+             return Connect(databaseName, username, password);
+        }
+
+
+        private static IMongoDatabase Connect(string databaseName, string username = default, string password = default)
         {
             username = username ?? Environment.GetEnvironmentVariable("COMPLEXITY_MONGO_USERNAME");
             password = password ?? Environment.GetEnvironmentVariable("COMPLEXITY_MONGO_PASSWORD");
-            var client = new MongoClient
-            ($"mongodb://{username}:{password}@quizcluster-shard-00-00-kzjb8.azure.mongodb.net:27017," +
-             "quizcluster-shard-00-01-kzjb8.azure.mongodb.net:27017," +
-             "quizcluster-shard-00-02-kzjb8.azure.mongodb.net:27017/" +
-             $"{databaseName}?ssl=true&replicaSet=QuizCluster-shard-0&authSource=admin&retryWrites=true");
+            var client =
+                new
+                    MongoClient($"mongodb://{username}:{password}@quizcluster-shard-00-00-kzjb8.azure.mongodb.net:27017," +
+                                "quizcluster-shard-00-01-kzjb8.azure.mongodb.net:27017," +
+                                "quizcluster-shard-00-02-kzjb8.azure.mongodb.net:27017/" +
+                                $"{databaseName}?ssl=true&replicaSet=QuizCluster-shard-0&authSource=admin&retryWrites=true");
             return client.GetDatabase("test");
         }
 
-        public static void SetupDatabase()
+        private static void SetupDatabase()
         {
             RegisterClassMap<Entity<Guid>>(cm =>
             {
                 cm.AutoMap();
-                cm.MapMember(c => c.Id);
+                cm.MapIdMember(c => c.Id);
                 cm.AddKnownType(typeof(Entity));
             });
 
             RegisterClassMap<Entity>(cm =>
             {
                 cm.AutoMap();
-                cm.AddKnownType(typeof(LevelProgressEntity));
-                cm.AddKnownType(typeof(TaskInfoEntity));
-                cm.AddKnownType(typeof(TopicProgressEntity));
-                cm.AddKnownType(typeof(UserEntity));
-                cm.AddKnownType(typeof(UserProgressEntity));
-                cm.AddKnownType(typeof(TaskGenerator));
-                cm.AddKnownType(typeof(Level));
-                cm.AddKnownType(typeof(Topic));
                 cm.SetIsRootClass(true);
             });
 
-            RegisterClassMap<Level>(cm =>
-            {
-                cm.MapMember(c => c.Description);
-                cm.MapMember(c => c.Generators);
-                cm.MapMember(c => c.NextLevels);
-                cm.MapCreator(c => new Level(c.Id, c.Description, c.Generators, c.NextLevels));
-            });
-            RegisterClassMap<Topic>(cm =>
-            {
-                cm.MapMember(c => c.Description);
-                cm.MapMember(c => c.Levels);
-                cm.MapMember(c => c.Name);
+            AutoRegisterClassMap<Level>(c => new Level(c.Id, c.Description, c.Generators, c.NextLevels));
+            AutoRegisterClassMap<Topic>(c => new Topic(c.Id, c.Name, c.Description, c.Levels));
 
-                cm.MapCreator(c => new Topic(c.Id, c.Name, c.Description, c.Levels));
-            });
+            AutoRegisterClassMap<TemplateTaskGenerator>(c => new TemplateTaskGenerator(c.Id, c.PossibleAnswers,
+                                                                                       c.TemplateCode, c.Hints,
+                                                                                       c.Answer, c.Streak));
+            AutoRegisterClassMap<TaskGenerator>(cm => cm.SetIsRootClass(true));
+            AutoRegisterClassMap<TaskInfoEntity>(c => new TaskInfoEntity(c.Question, c.Answer, c.Hints, c.HintsTaken,
+                                                                         c.ParentGeneratorId, c.IsSolved, c.Id));
 
-            RegisterClassMap<TemplateTaskGenerator>(cm =>
-            {
-                cm.MapMember(c => c.Answer);
-                cm.MapMember(c => c.Hints);
-                cm.MapMember(c => c.PossibleAnswers);
-                cm.MapMember(c => c.TemplateCode);
+            AutoRegisterClassMap<UserEntity>(c => new UserEntity(c.Id, c.UserProgressEntity));
 
-                cm.MapCreator(c => new TemplateTaskGenerator(c.Id, c.PossibleAnswers, c.TemplateCode, c.Hints, c.Answer,
-                                                             c.Streak));
-            });
-            RegisterClassMap<TaskGenerator>(cm =>
+            AutoRegisterClassMap<LevelProgressEntity>(cm =>
             {
-                cm.MapMember(c => c.Streak);
-                cm.SetIsRootClass(true);
-            });
-
-            RegisterClassMap<TaskInfoEntity>(cm =>
-            {
-                cm.MapMember(c => c.Answer);
-                cm.MapMember(c => c.Hints);
-                cm.MapMember(c => c.HintsTaken);
-                cm.MapMember(c => c.IsSolved);
-                cm.MapMember(c => c.ParentGeneratorId);
-                cm.MapMember(c => c.Question);
-                cm.MapCreator(c => new TaskInfoEntity(c.Question, c.Answer, c.Hints, c.HintsTaken, c.ParentGeneratorId, c.IsSolved, c.Id));
-            });
-            RegisterClassMap<UserEntity>(cm =>
-            {
-                cm.MapMember(c => c.UserProgressEntity);
-                cm.MapCreator(c => new UserEntity(c.Id, c.UserProgressEntity));
-            });
-
-            RegisterClassMap<LevelProgressEntity>(cm =>
-            {
-                cm.MapMember(c => c.LevelId);
-                cm.MapMember(c => c.CurrentLevelStreaks)
-                  .SetSerializer(new DictionaryInterfaceImplementerSerializer<Dictionary<Guid, int>
-                                 >(DictionaryRepresentation.ArrayOfDocuments));
+                cm.MapDictionary(c => c.CurrentLevelStreaks);
                 cm.MapCreator(c => new LevelProgressEntity(c.LevelId, c.CurrentLevelStreaks, c.Id));
-
             });
-            RegisterClassMap<TopicProgressEntity>(cm =>
+            AutoRegisterClassMap<TopicProgressEntity>(cm =>
             {
-                cm.MapMember(c => c.TopicId);
-                cm.MapMember(c => c.LevelProgressEntities)
-                  .SetSerializer(new DictionaryInterfaceImplementerSerializer<Dictionary<Guid, LevelProgressEntity>
-                                 >(DictionaryRepresentation.ArrayOfDocuments));
+                cm.MapDictionary(c => c.LevelProgressEntities);
                 cm.MapCreator(c => new TopicProgressEntity(c.LevelProgressEntities, c.TopicId, c.Id));
             });
-            RegisterClassMap<UserProgressEntity>(cm =>
+            AutoRegisterClassMap<UserProgressEntity>(cm =>
             {
-                cm.MapMember(c => c.CurrentLevelId);
-                cm.MapMember(c => c.CurrentTask);
-                cm.MapMember(c => c.CurrentTopicId);
-                cm.MapMember(c => c.UserId);
-                cm.MapMember(c => c.TopicsProgress)
-                  .SetSerializer(new DictionaryInterfaceImplementerSerializer<Dictionary<Guid, TopicProgressEntity>
-                                 >(DictionaryRepresentation.ArrayOfDocuments));
+                cm.MapDictionary(c => c.TopicsProgress);
                 cm.MapCreator(c => new UserProgressEntity(c.CurrentTopicId, c.CurrentLevelId, c.UserId,
                                                           c.TopicsProgress, c.CurrentTask, c.Id));
+            });
+        }
+
+        private static BsonMemberMap MapDictionary<TClass, TKey, TValue>(
+            this BsonClassMap<TClass> cm,
+            Expression<Func<TClass, Dictionary<TKey, TValue>>> memberLambda) =>
+            cm.MapMember(memberLambda)
+              .SetSerializer(new DictionaryInterfaceImplementerSerializer<Dictionary<TKey, TValue>
+                             >(DictionaryRepresentation.ArrayOfDocuments));
+
+        private static void AutoRegisterClassMap<TClass>(Expression<Func<TClass, TClass>> creatorLambda) =>
+            AutoRegisterClassMap<TClass>(cm => cm.MapCreator(creatorLambda));
+
+        private static void AutoRegisterClassMap<T>(Action<BsonClassMap<T>> additionalAction = null)
+        {
+            RegisterClassMap<T>(cm =>
+            {
+                var propertyInfos = typeof(T)
+                                    .GetProperties(BindingFlags.DeclaredOnly |
+                                                   BindingFlags.Public |
+                                                   BindingFlags.Instance)
+                                    .Cast<MemberInfo>()
+                                    .ToList();
+                foreach (var propertyInfo in propertyInfos) cm.MapMember(propertyInfo);
+                additionalAction?.Invoke(cm);
             });
         }
     }

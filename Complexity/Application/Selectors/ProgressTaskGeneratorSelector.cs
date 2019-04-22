8577b56f6aad898e692899ec0c609456d4649744
Artemiy <art.izakov@gmail.com>
2019-04-22 19:54:23 +0500

    add new selectors

---
+++ Complexity/Application/Selectors/ProgressTaskGeneratorSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Application.Repositories;
+using Application.Repositories.Entities;
+using Domain.Entities.TaskGenerators;
+
+namespace Application.Selectors
+{
+    public class ProgressTaskGeneratorSelector : ITaskGeneratorSelector
+    {
+        private readonly ITaskRepository taskRepository;
+
+        public ProgressTaskGeneratorSelector(ITaskRepository taskRepository)
+        {
+            this.taskRepository = taskRepository;
+        }
+
+        public TaskGenerator Select(IEnumerable<TaskGenerator> generators, UserProgressEntity progress)
+        {
+            
+        }
+    }
+}
---
+++ Complexity/Application/Selectors/RandomTaskGeneratorSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Application.Repositories.Entities;
+using Domain.Entities.TaskGenerators;
+
+namespace Application.Selectors
+{
+    public class RandomTaskGeneratorSelector : ITaskGeneratorSelector
+    {
+        private readonly Random random;
+
+        public RandomTaskGeneratorSelector(Random random)
+        {
+            this.random = random;
+        }
+
+        public TaskGenerator Select(IEnumerable<TaskGenerator> generators, UserProgressEntity progress)
+        {
+            var generatorsArray = generators.ToArray();
+            return generatorsArray.Length == 0 ? null : generatorsArray[random.Next(generatorsArray.Length)];
+        }
+    }
+}

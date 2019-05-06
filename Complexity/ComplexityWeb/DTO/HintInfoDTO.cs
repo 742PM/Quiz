b70698e89ccf636805ff3d3782e00a6c37159e97
Artemiy <art.izakov@gmail.com>
2019-05-06 20:12:45 +0500

    add new dto classes

---
+++ Complexity/ComplexityWeb/DTO/HintInfoDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ComplexityWebApi.DTO
+{
+    public class HintInfoDTO
+    {
+        public HintInfoDTO(string hintText, bool hasNext)
+        {
+            HintText = hintText;
+            HasNext = hasNext;
+        }
+
+        [JsonProperty("hint_text")] public string HintText { get; }
+        [JsonProperty("has_next")] public bool HasNext { get; }
+    }
+}

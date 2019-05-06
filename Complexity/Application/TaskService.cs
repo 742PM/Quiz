269ebb1cdf41b875b00c84297ff7183db6538b50
Artemiy <art.izakov@gmail.com>
2019-05-06 20:38:49 +0500

    add implementation

--- Complexity/Application/TaskService.cs
+++ Complexity/Application/TaskService.cs
@@ -1,23 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Application.Repositories;
+using Domain.Entities;
+using Domain.Entities.TaskGenerators;
 using Domain.Values;
 using Infrastructure.Result;
 
-namespace Application {
-    class TaskService : ITaskService {
-        public IEnumerable<Task> GetAllTopics()
+namespace Application
+{
+    class TaskService : ITaskService
+    {
+        private readonly ITaskRepository taskRepository;
+        private readonly Random random;
+
+        public TaskService(ITaskRepository taskRepository, Random random)
         {
-            throw new NotImplementedException();
+            this.taskRepository = taskRepository;
+            this.random = random;
         }
+
+        public IEnumerable<Topic> GetAllTopics() => taskRepository.GetTopics();
 
         public Guid AddEmptyTopic(string name, string description)
         {
-            throw new NotImplementedException();
+            return taskRepository
+                .InsertTopic(new Topic(Guid.NewGuid(), name, description, new Level[0]))
+                .Id;
         }
 
-        public Result<Guid, Exception> AddEmptyLevel(Guid topicId, string description, IEnumerable<Guid> previousLevels, IEnumerable<Guid> nextLevels)
+        public Result<Guid, Exception> AddEmptyLevel(
+            Guid topicId,
+            string description,
+            IEnumerable<Guid> previousLevels,
+            IEnumerable<Guid> nextLevels)
         {
-            throw new NotImplementedException();
+            return taskRepository
+                .InsertLevel(
+                    topicId,
+                    new Level(Guid.NewGuid(), description, new TaskGenerator[0], new Guid[0]))
+                .Id;
         }
 
         public Guid AddTemplateGenerator(
@@ -29,12 +51,34 @@ namespace Application {
             IEnumerable<string> hints,
             int streak)
         {
-            throw new NotImplementedException();
+            var possibleAnswerArray = possibleAnswers as string[] ?? possibleAnswers.ToArray();
+            var hintsArray = hints as string[] ?? hints.ToArray();
+
+            return taskRepository
+                .InsertGenerator(
+                    topicId,
+                    levelId,
+                    new TemplateTaskGenerator(
+                        Guid.NewGuid(),
+                        possibleAnswerArray,
+                        template,
+                        hintsArray,
+                        rightAnswer,
+                        streak))
+                .Id;
         }
 
-        public Task RenderTask(string template, IEnumerable<string> possibleAnswers, string rightAnswer, IEnumerable<string> hints)
+        public Task RenderTask(
+            string template,
+            IEnumerable<string> possibleAnswers,
+            string rightAnswer,
+            IEnumerable<string> hints)
         {
-            throw new NotImplementedException();
+            var possibleAnswerArray = possibleAnswers as string[] ?? possibleAnswers.ToArray();
+            var hintsArray = hints as string[] ?? hints.ToArray();
+
+            return new TemplateTaskGenerator(Guid.Empty, possibleAnswerArray, template, hintsArray, rightAnswer, 1)
+                .GetTask(random);
         }
     }
 }

32235aaa84c4a3510d922ae1a1f71c6bd0cfc15c
Artemiy <art.izakov@gmail.com>
2019-05-06 19:41:43 +0500

    add new info classes

---
+++ Complexity/Application/Info/LevelProgressInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Info
+{
+    public class LevelProgressInfo
+    {
+        public LevelProgressInfo(int tasksCount, int tasksSolved)
+        {
+            TasksCount = tasksCount;
+            TasksSolved = tasksSolved;
+        }
+
+        public int TasksCount { get; }
+        public int TasksSolved { get; }
+    }
+}

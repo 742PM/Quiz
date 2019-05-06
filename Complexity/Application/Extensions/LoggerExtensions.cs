efb4d0c611283e77ffc0f0ecbd906ab501acdaeb
Artemiy <art.izakov@gmail.com>
2019-05-06 19:26:06 +0500

    create service class

--- Complexity/Application/Extensions/LoggerExtensions.cs
+++ Complexity/Application/Extensions/LoggerExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.Logging;
 
-namespace Application
+namespace Application.Extensions
 {
     public static class LoggerExtensions
     {
---
+++ Complexity/Application/TaskService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Values;
+using Infrastructure.Result;
+
+namespace Application {
+    class TaskService : ITaskService {
+        public IEnumerable<Task> GetAllTopics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddEmptyTopic(string name, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<Guid, Exception> AddEmptyLevel(Guid topicId, string description, IEnumerable<Guid> previousLevels, IEnumerable<Guid> nextLevels)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid AddTemplateGenerator(
+            Guid topicId,
+            Guid levelId,
+            string template,
+            IEnumerable<string> possibleAnswers,
+            string rightAnswer,
+            IEnumerable<string> hints,
+            int streak)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RenderTask(string template, IEnumerable<string> possibleAnswers, string rightAnswer, IEnumerable<string> hints)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

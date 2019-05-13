2d2a449eb4e03e6043c11eaf1b713b20fc577c90
funfunfine <grygry0@gmail.com>
2019-05-13 21:39:37 +0500

    create develop branch;

--- Quiz/QuizWebHookBot/Startup.cs
+++ Quiz/QuizWebHookBot/Startup.cs
@@ -26,7 +26,6 @@ namespace QuizWebHookBot
             new MongoClient(connectionString).GetDatabase(DatabaseName);
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);

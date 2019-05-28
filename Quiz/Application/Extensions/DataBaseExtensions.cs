211a57aa04d438bb5d90cbe24e9a6c98cb91ae0d
Artemiy <art.izakov@gmail.com>
2019-05-28 18:54:07 +0500

    move method to extensions

--- Quiz/Application/Extensions/DataBaseExtensions.cs
+++ Quiz/Application/Extensions/DataBaseExtensions.cs
@@ -166,5 +166,7 @@ namespace Application.Extensions
 
             userRepository.Update(user);
         }
+
+        public static bool CurrentTaskExists(this UserEntity user) => user.UserProgressEntity.CurrentTask != null;
     }
 }

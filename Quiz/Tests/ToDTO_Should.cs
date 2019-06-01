f21749b708651cb695a8108d3cdc89a133b4329b
funfunfine <grygry0@gmail.com>
2019-06-01 08:47:54 +0500

    cleanup;

--- Quiz/Tests/ToDTO_Should.cs
+++ Quiz/Tests/ToDTO_Should.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Application.DTO;
 using Application.Extensions;
@@ -8,10 +7,20 @@ using Domain.Entities.TaskGenerators;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace Tests {
+namespace Tests
+{
     [TestFixture]
     public class ToDTO_Should
     {
+        [Test]
+        public void CastEmptyLevels()
+        {
+            new Topic(Guid.NewGuid(), "t", "opic", new Level[0])
+                .ToDTO()
+                .Should()
+                .BeEquivalentTo(new TopicDto { Name = "t", Description = "opic", Levels = new LevelDto[0] });
+        }
+
         [Test]
         public void CastNextLevels_ToInt()
         {
@@ -20,26 +29,21 @@ namespace Tests {
                          {
                              new Level(ids[0], "a", new TaskGenerator[0], new[] { ids[1] }),
                              new Level(ids[1], "b", new TaskGenerator[0], new[] { ids[2], ids[3] }),
-                             new Level(ids[2],"c",new TaskGenerator[0],new Guid[0]  ),
-                             new Level(ids[3],"d",new TaskGenerator[0],new Guid[0]  )
+                             new Level(ids[2], "c", new TaskGenerator[0], new Guid[0]),
+                             new Level(ids[3], "d", new TaskGenerator[0], new Guid[0])
                          };
             new Topic(Guid.NewGuid(), "t", "opic", levels)
                 .ToDTO()
-                .Levels.Select(l => (n:l.Number, nl:l.NextLevels)).ToArray()
+                .Levels.Select(l => (n: l.Number, nl: l.NextLevels))
+                .ToArray()
                 .Should()
                 .BeEquivalentTo(new[]
                                 {
-                                    (n:0,new[]{1}),
-                                    (n:1,new[]{2,3}),
-                                    (n:2,new int[0]),
-                                    (n:3,new int[0]),
+                                    (n: 0, new[] { 1 }),
+                                    (n: 1, new[] { 2, 3 }),
+                                    (n: 2, new int[0]),
+                                    (n: 3, new int[0])
                                 });
-        }
-
-        [Test]
-        public void CastEmptyLevels()
-        {
-            new Topic(Guid.NewGuid(), "t", "opic", new Level[0]).ToDTO().Should().BeEquivalentTo(new TopicDto(){Name="t",Description = "opic",Levels = new LevelDto[0]});
         }
     }
 }

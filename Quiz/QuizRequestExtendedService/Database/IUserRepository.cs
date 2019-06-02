607a176dd4f349c8d4cc63e4d2c65d52503e5831
funfunfine <grygry0@gmail.com>
2019-06-02 16:20:28 +0500

    reformat;

--- Quiz/QuizRequestExtendedService/Database/IUserRepository.cs
+++ Quiz/QuizRequestExtendedService/Database/IUserRepository.cs
@@ -3,11 +3,12 @@ using Infrastructure.Result;
 
 namespace QuizRequestExtendedService.Database
 {
-
     public interface IUserRepository<TToken, in TAuthData>
     {
         Task RegisterUserAsync(TAuthData data);
+
         Task<Maybe<TToken>> AuthenticateUserAsync(TAuthData data);
+
         None RegisterUser(TAuthData data);
 
         Maybe<TToken> AuthenticateUser(TAuthData data);

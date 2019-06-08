d044086aa1c82ea213b702b6827f0a56b61ff22f
funfunfine <grygry0@gmail.com>
2019-06-08 21:07:24 +0500

    implement telegram logger;

--- Quiz/ComplexityWeb/Program.cs
+++ Quiz/ComplexityWeb/Program.cs
@@ -1,21 +1,20 @@
-﻿using Microsoft.AspNetCore;
+﻿using Infrastructure.Logger;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace QuizWebApp
 {
-#pragma warning disable CS1591
+
     public static class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateWebHostBuilder(args)
                 .Build()
                 .Run();
-        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                   .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).ConfigureLogging(b => b.AddTelegram())
+                .UseStartup<Startup>();
     }
-#pragma warning restore CS1591
+
 }
